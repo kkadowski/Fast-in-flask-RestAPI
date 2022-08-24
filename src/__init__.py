20f10f09f086c67c0d7ddfffb8ac75cd3df760c3
Krzysztof Kadowski <kkadowski@gmail.com>
2022-08-24 12:05:32 +0200

    Database setup

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify
 import os
 from src.auth import auth
 from src.bookmarks  import bookmarks
+from src.database import db
+
 
 def create_app(test_config=None):
     app = Flask(__name__, 
@@ -11,10 +13,20 @@ def create_app(test_config=None):
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY = os.environ.get("SECRET_KEY"),
+            SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DB_URI"),
+            SQLALCHEMY_TRACK_MODIFICATIONS = False,
         ) 
     else:
         app.config.from_mapping(test_config)
-        
+     
+    db.app=app   
+    db.init_app(app)
+    #flask shell
+    #from src.database import sb
+    #db.create_all()
+    #db
+    #db.drop_all() - usuwanie
+    
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
     
